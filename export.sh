d93ce582e704704260e4ed8002c8a195dcbe9a09
Insality <insality@gmail.com>
2020-10-11 17:00:07 +0300

    Update emmylua ldoc generation

---
+++ export.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+### Exit on Cmd+C / Ctrl+C
+trap "exit" INT
+# trap clean EXIT
+set -e
+
+dump_file="./_temporary_ldoc.dump"
+output_file="./annotations.lua"
+
+# clean() {
+    #rm -f ${dump_file}
+# }
+
+
+### Generate dump file
+echo "Make ldoc from $1"
+echo "return" > ./test.dump
+cwd=$(pwd)
+cd $1
+echo "Current cwd $(pwd)"
+dump_result="$(ldoc . --filter pl.pretty.dump >> ${cwd}/test.dump)"
+cd $cwd
+
+### Generate annotations
+echo "Generate annotations from ldoc to annotations.lua"
+lua main.lua ./test.dump > annotation.lua
+
--- main.lua
+++ main.lua
@@ -1,4 +1,3 @@
-
 local function split(inputstr, sep)
 	sep = sep or "%s"
 	local t = {}
@@ -27,6 +26,21 @@ local function replace_new_line(s)
 end
 
 
+---@class ldoc_structure
+---@field functions ldoc_function_structure[]
+---@field name string
+---@field submodules table<name, boolean>
+
+---@class ldoc_function_structure
+---@field args ldoc_args_structure[]
+---@field args_string string
+---@field desc string
+---@field name string
+---@field return_values ldoc_return_structure[]
+
+---@class ldoc_args_structure [1] - name, [2] - type, [3] - desc
+
+---@class ldoc_return_structure [1] - type, [2] - desc
 
 local function parse_module(parsed_data, module_data)
 	local class = {}
@@ -86,34 +100,188 @@ local function parse_module(parsed_data, module_data)
 end
 
 
+local function get_module_name(function_name, base_module)
+	local names = split(function_name, ".")
+	if #names > 1 then
+		names[#names] = nil
+		return table.concat(names, ".")
+	else
+		return base_module
+	end
+end
+
+
+local function get_function_name(function_name)
+	local names = split(function_name, ".")
+	if #names > 1 then
+		return names[#names]
+	else
+		return function_name
+	end
+end
+
+
+---@param prepared_data prepared_structure
+---@param module_name string
+local function check_module(prepared_data, module_name)
+	if not prepared_data.modules[module_name] then
+		prepared_data.modules[module_name] = {
+			functions = {},
+			fields = {}
+		}
+	end
+
+	local upper_module = get_module_name(module_name, module_name)
+
+	if upper_module ~= module_name then
+		check_module(prepared_data, upper_module)
+
+		local field_name = get_function_name(module_name)
+
+		local is_exist = false
+		for i = 1, #prepared_data.modules[upper_module].fields do
+			if prepared_data.modules[upper_module].fields[i].name == field_name then
+				is_exist = true
+			end
+		end
+
+		if not is_exist then
+			---@type prepared_field_info
+			local field_info = {}
+			field_info.name = field_name
+			field_info.type = module_name
+			field_info.desc = "Submodule"
+			table.insert(prepared_data.modules[upper_module].fields, field_info)
+		end
+	end
+end
+
+
+---@param args ldoc_args_structure[]
+---@return prepared_field_info[]
+local function parse_args(args)
+	---@type prepared_field_info[]
+	local parsed = {}
+
+	for i = 1, #args do
+		---@type prepared_field_info
+		local info = {}
+		info.name = args[i][1]
+		info.type = args[i][2]
+		info.desc = replace_new_line(trim(args[i][3]))
+		table.insert(parsed, info)
+	end
+
+	return parsed
+end
+
+---@param args ldoc_return_structure[]
+---@return prepared_field_info[]
+local function parse_return_values(return_values)
+	---@type prepared_field_info[]
+	local parsed = {}
+
+	for i = 1, #return_values do
+		---@type prepared_field_info
+		local info = {}
+		info.type = return_values[i][1]
+		info.desc = replace_new_line(trim(return_values[i][2]))
+		table.insert(parsed, info)
+	end
+
+	return parsed
+end
+
+
+---@class prepared_structure
+---@field modules table<string, prepared_module_structure>
+
+---@class prepared_module_structure
+---@field functions prepared_function_structure[]
+---@field fields prepared_field_info[]
+
+---@class prepared_function_structure
+---@field name string
+---@field desc string
+---@field args prepared_field_info[]
+---@field return_value prepared_field_info[]
+
+---@class prepared_field_info
+---@field name string
+---@field type string
+---@field desc string
+
+
+---@param parsed_data table<string, ldoc_structure>
+---@return prepared_structure
+local function prepare_data(parsed_data)
+	---@type prepared_structure
+	local prepared = {
+		modules = {}
+	}
+
+	for class_name, data in pairs(parsed_data) do
+		for index, function_data in pairs(data.functions) do
+			---@type prepared_function_structure
+			local function_info = {}
+
+			local module_name = get_module_name(function_data.name, class_name)
+			local function_name = get_function_name(function_data.name)
+			check_module(prepared, module_name)
+
+			function_info.name = function_name
+			function_info.desc = trim(replace_new_line(function_data.desc))
+			function_info.args = parse_args(function_data.args)
+			function_info.return_value = parse_return_values(function_data.return_values)
+
+			table.insert(prepared.modules[module_name].functions, function_info)
+		end
+	end
+
+	return prepared
+end
+
+
+---@param data prepared_structure
 local function make_annotations(data)
 	local result = ""
 
-	for class_name, class_data in pairs(data) do
+	for class_name, class_structure in pairs(data.modules) do
 		result = result .. "---@class " .. class_name .. "\n"
-		result = result .. "local " .. class_name .. " = {}\n"
-
-		for submodule, _ in pairs(class_data.submodules) do
-			result = result .. class_name .. "." .. submodule .. " = {}\n"
+		for i = 1, #class_structure.fields do
+			-- TODO: fill fields
+			local field = class_structure.fields[i]
+			local field_string = string.format("---@field %s %s %s", field.name, field.type, field.desc)
+			result = result .. field_string .. "\n"
 		end
+
+		for _, function_info in pairs(class_structure.functions) do
+			local args_string = ""
+			for i = 1, #function_info.args do
+				local arg = function_info.args[i]
+				local arg_string = string.format("%s:%s", arg.name, arg.type)
+				args_string = args_string .. arg_string
+
+				if i < #function_info.args then
+					args_string = args_string .. ", "
+				end
+			end
+
+			local return_string = #function_info.return_value > 0 and ":" or ""
+			for i = 1, #function_info.return_value do
+				local arg = function_info.return_value[i]
+				return_string = return_string .. arg.type
+
+				if i < #function_info.return_value then
+					return_string = return_string .. ", "
+				end
+			end
+
+			local field_string = string.format("---@field %s fun(%s)%s %s", function_info.name, args_string, return_string, function_info.desc)
+			result = result .. field_string .. "\n"
+		end
+
 		result = result .. "\n"
-
-		for i = 1, #class_data.functions do
-			local function_data = class_data.functions[i]
-
-			local formatted_desc = split(function_data.desc, "\n")
-			for j = 1, #formatted_desc do
-				result = result .. "---" .. trim(formatted_desc[j]) .. "\n"
-			end
-
-			for _, arg in ipairs(function_data.args) do
-				result = result .. "---@param " .. arg[1] .. " " .. arg[2] .. " " .. arg[3] .. "\n"
-			end
-			for _, ret in ipairs(function_data.return_values) do
-				result = result .. "---@return " .. ret[1] .. " " .. ret[2] .. "\n"
-			end
-			result = result .. "function " .. function_data.name .. "(" .. function_data.args_string .. ") end\n\n"
-		end
 	end
 
 	return result
@@ -126,9 +294,10 @@ local function main()
 	for _, ldoc_module in pairs(filedata) do
 		parse_module(parsed_data, ldoc_module)
 	end
-	local annotations = make_annotations(parsed_data)
+
+	local prepared_data = prepare_data(parsed_data) -- Transform ldoc parsed data to our structure
+	local annotations = make_annotations(prepared_data) -- Output our structure to string
 	print(annotations)
 end
-
 
 main()
